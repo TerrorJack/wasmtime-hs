c7348e35ceec6168f30ca238734a1d9c74b7cdff
Cheng Shao <cheng.shao@tweag.io>
2021-04-25 08:59:30 +0000

    Ensure cbits work as well

---
+++ cbits/cbits.c
@@ -0,0 +1,3 @@
+#include <wasm.h>
+
+wasm_engine_t *my_wasm_engine_new(void) { return wasm_engine_new(); }
--- test/hs-wasmtime-test.hs
+++ test/hs-wasmtime-test.hs
@@ -3,4 +3,4 @@ import Foreign
 main :: IO ()
 main = print =<< c_wasm_engine_new
 
-foreign import ccall unsafe "wasm_engine_new" c_wasm_engine_new :: IO (Ptr ())
+foreign import ccall unsafe "my_wasm_engine_new" c_wasm_engine_new :: IO (Ptr ())
