3bc428526d00d706e02ec45c4082edc0218965b2
Cheng Shao <cheng.shao@tweag.io>
2021-06-04 11:07:38 +0000

    WIP

--- nix/hs4nix.nix
+++ nix/hs4nix.nix
@@ -1,5 +1,4 @@
 { sources ? import ./sources.nix { }
-, haskellNix ? import sources.haskell-nix { }
 , pkgs ? import sources.nixpkgs { overlays = [ (import ./wasmtime-c-api.nix) ]; }
 }:
 with pkgs;
@@ -7,6 +6,14 @@ mkShell {
   nativeBuildInputs = [
     cabal-install
     (haskellPackages.ghcWithPackages (ps: with ps; [ unliftio vector ]))
+    (import
+      (fetchFromGitHub {
+        owner = "NixOS";
+        repo = "nixpkgs";
+        rev = "5730083206024c17d41fa33a88538bc84246284b";
+        sha256 = "sha256-fxzQ6w1pe+4xEjH2f/HrnUt9Pd/fDBeNmxAAtc2npmM=";
+      })
+      { }).netlify-cli
   ];
   buildInputs = [ wasmtime ];
 }
